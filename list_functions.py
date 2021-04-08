190b42ab29ef304d3d143d9dd4ac455604106d9d
M.Umair <75035403+umair1440@users.noreply.github.com>
2021-04-08 16:01:30 +0500

    Add files via upload

---
+++ list_functions.py
@@ -0,0 +1,111 @@
+
+
+
+# 	APPEND FUNCTION OF THE LIST
+# this function is used to add value in the list 
+
+firstList = []
+firstList.append("hello")
+print(firstList)
+
+
+
+# 	CLEAR FUNCTION OF THE LIST
+# clear function is used to clear the list
+
+secondlist=["1","2","3","4"]
+secondlist.clear()
+print(secondlist)
+
+
+
+# COUNT FUNCTION OF THE LIST 
+# count function is used to count letter from the list
+
+thirdlist=["apple","mango","banana"]
+x=thirdlist.count("apple")
+print(x)
+
+
+# EXTEND FUNCTION OF THE LIST
+# extend function is used to extend the list with another list
+
+fourthlist=["love","hate","good"]
+cars=["BMW","bhugatti","FORD"] 
+fourthlist.extend(cars)
+print(fourthlist)
+
+
+
+# INDEX FUNCTION OF THE LIST 
+# index function is used to find postion of the letter
+
+fifthlist = ["first","second","third","fourth"]
+z=fifthlist.index("second")
+print(z)
+
+
+# INSERT FUNCTION OF THE LIST
+# insert function is used to enter the value in list
+
+sixthlist= ["hello","world"]
+sixthlist.insert(1, "new")
+print(sixthlist)
+
+
+# POP FUNCTION OF THE LIST
+# pop function is used to delete a value from the list with its index
+
+seventhlist = ["My","Name","is","umair"]
+seventhlist.pop(2)
+print(seventhlist)
+
+
+# REMOVEFUNCTION OF THE LIST
+#  remove function is used to delete a value from the list with its name
+
+eighthlist = ["Her", "name" , "is" , "hira"]
+eighthlist.remove("is")
+print(eighthlist)
+
+
+# REVERSE FUNCTION OF THE LIST
+# reverse function is used to reverse the list value postions
+
+ninthlist = ["none" , "of" , "your" , "Business"]
+ninthlist.reverse()
+print(ninthlist)
+
+
+# SORT FUNCTION OF THE LIST
+# sort function is used for sorting the value of the list
+
+
+tenthlist = ["3" , "4" , "2"]
+tenthlist.sort()
+print(tenthlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
